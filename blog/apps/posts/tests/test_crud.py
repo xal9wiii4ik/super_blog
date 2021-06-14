cef228add1b1592fa5e4e3f400da118ffafb71d4
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-06-14 16:47:10 +0300

    add tests for views and for serializers'

---
+++ blog/apps/posts/tests/test_crud.py
@@ -0,0 +1,167 @@
+import json
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.db.models import F
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+from apps.posts.models import Post, Category
+from apps.posts.serializers import PostModelSerializer, CategoryModelSerializer
+
+
+class CategoryApiTestCase(APITestCase):
+    """
+    Api test for categories
+    """
+
+    def setUp(self) -> None:
+        self.category = Category.objects.create(name='category')
+        self.category_1 = Category.objects.create(name='category_1')
+
+    def test_get(self) -> None:
+        """
+        Get categories
+        """
+
+        url = reverse('post:category-list')
+        categories = Category.objects.all()
+        response = self.client.get(path=url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(CategoryModelSerializer(categories, many=True).data,
+                         response.data)
+
+    def test_get_retrieve(self) -> None:
+        """
+        Get one category
+        """
+
+        url = reverse('post:category-detail', args=(self.category.pk,))
+        response = self.client.get(path=url)
+        self.assertEqual(first=status.HTTP_200_OK, second=response.status_code)
+        self.assertEqual(CategoryModelSerializer(self.category).data,
+                         response.data)
+
+    def test_create(self) -> None:
+        """
+        Create new category
+        """
+
+        url = reverse('post:category-list')
+        data = {
+            'name': 'something new'
+        }
+        json_data = json.dumps(data)
+        response = self.client.post(path=url, data=json_data,
+                                    content_type='application/json')
+        self.assertEqual(first=status.HTTP_405_METHOD_NOT_ALLOWED, second=response.status_code)
+
+    def test_delete(self) -> None:
+        """
+        Delete category
+        """
+
+        url = reverse('post:category-detail', args=(self.category.pk,))
+        response = self.client.delete(path=url)
+        self.assertEqual(first=status.HTTP_405_METHOD_NOT_ALLOWED, second=response.status_code)
+
+    def test_update(self) -> None:
+        """
+        Update category
+        """
+
+        url = reverse('post:category-detail', args=(self.category.pk,))
+        data = {
+            'name': 'new name'
+        }
+        json_data = json.dumps(data)
+        response = self.client.put(path=url, data=json_data,
+                                   content_type='application/json')
+        self.assertEqual(first=status.HTTP_405_METHOD_NOT_ALLOWED, second=response.status_code)
+
+
+class PostApiTestCase(APITestCase):
+    """
+    Api test for post
+    """
+
+    def setUp(self) -> None:
+        self.category = Category.objects.create(name='category')
+        self.category_1 = Category.objects.create(name='category_1')
+
+        self.post = Post.objects.create(title='first title for posts',
+                                        description='description for first post',
+                                        category=self.category_1)
+        self.post_1 = Post.objects.create(title='this is the second title for posts',
+                                          description='this is description for second post',
+                                          category=self.category)
+
+    def test_get(self) -> None:
+        """
+        Get posts
+        """
+
+        url = reverse('post:post-list')
+        categories = Post.objects.all()
+        response = self.client.get(path=url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(PostModelSerializer(categories, many=True).data,
+                         response.data)
+
+    def test_get_retrieve(self) -> None:
+        """
+        Get one post
+        """
+
+        url = reverse('post:post-detail', args=(self.post.pk,))
+        response = self.client.get(path=url)
+        self.assertEqual(first=status.HTTP_200_OK, second=response.status_code)
+        self.assertEqual(PostModelSerializer(self.post).data,
+                         response.data)
+
+    def test_create(self) -> None:
+        """
+        Create new post
+        """
+
+        self.assertEqual(first=Post.objects.all().count(), second=2)
+        url = reverse('post:post-list')
+        data = {
+            'title': 'title',
+            'description': 'description',
+            'category': self.category.pk
+        }
+        json_data = json.dumps(data)
+        response = self.client.post(path=url, data=json_data,
+                                    content_type='application/json')
+        self.assertEqual(first=status.HTTP_201_CREATED, second=response.status_code)
+        self.assertEqual(first=Post.objects.all().count(), second=3)
+
+    def test_delete(self) -> None:
+        """
+        Delete post
+        """
+
+        self.assertEqual(first=Post.objects.all().count(), second=2)
+        url = reverse('post:post-detail', args=(self.post.pk,))
+        response = self.client.delete(path=url)
+        self.assertEqual(first=status.HTTP_204_NO_CONTENT, second=response.status_code)
+        self.assertEqual(first=Post.objects.all().count(), second=1)
+
+    def test_update(self) -> None:
+        """
+        Update post
+        """
+
+        self.assertEqual(first=self.post.title, second='first title for posts')
+        url = reverse('post:post-detail', args=(self.post.pk,))
+        data = {
+            'title': 'new title'
+        }
+        json_data = json.dumps(data)
+        response = self.client.patch(path=url, data=json_data,
+                                   content_type='application/json')
+        self.assertEqual(first=status.HTTP_200_OK, second=response.status_code)
+        self.post.refresh_from_db()
+        self.assertEqual(first=self.post.title, second='new title')
---
+++ blog/apps/posts/tests/test_serializers.py
@@ -0,0 +1,65 @@
+from django.db.models import F
+from django.test import TestCase
+
+from apps.posts.models import Post, Category
+from apps.posts.serializers import CategoryModelSerializer, PostModelSerializer
+
+
+class SerializerTestCase(TestCase):
+    """
+    Test for serializers
+    """
+
+    def setUp(self) -> None:
+        self.category = Category.objects.create(name='category')
+        self.category_1 = Category.objects.create(name='category_1')
+        self.post = Post.objects.create(title='first title for posts',
+                                        description='description for first post',
+                                        category=self.category_1)
+        self.post_1 = Post.objects.create(title='this is the second title for posts',
+                                          description='this is description for second post',
+                                          category=self.category)
+
+    def test_category_serializer(self) -> None:
+        """
+        Test for serializer of category
+        """
+
+        categories = Category.objects.all()
+        data = CategoryModelSerializer(categories, many=True).data
+
+        expected_data = [
+            {
+                'id': self.category.pk,
+                'name': 'category'
+            },
+            {
+                'id': self.category_1.pk,
+                'name': 'category_1'
+            }
+        ]
+        self.assertEqual(expected_data, data)
+
+    def test_post_serializer(self) -> None:
+        """
+        Test for serializer of category
+        """
+
+        posts = Post.objects.all()
+        data = PostModelSerializer(posts, many=True).data
+
+        expected_data = [
+            {
+                'id': self.post.pk,
+                'title': 'first title for posts',
+                'description': 'description for first post',
+                'category': self.category_1.pk
+            },
+            {
+                'id': self.post_1.pk,
+                'title': 'this is the second title for posts',
+                'description': 'this is description for second post',
+                'category': self.category.pk
+            }
+        ]
+        self.assertEqual(first=data, second=expected_data)
