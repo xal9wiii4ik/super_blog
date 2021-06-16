f352c3a37e15aeb529d0f5916a675522721a277a
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-06-16 15:09:55 +0300

    add simple optimize images

--- blog/apps/posts/models.py
+++ blog/apps/posts/models.py
@@ -1,7 +1,26 @@
-from datetime import datetime
 import typing as tp
+import os
+
+from PIL import Image
+from datetime import datetime
 
 from django.db import models
+
+SIZE = (800, 768)
+
+
+def save_picture(image: tp.IO):
+    img = Image.open(image)
+    name = image.name
+    format = name.split('.')[-1]
+    # TODO format
+    if (img.size[0] < SIZE[0]) or (img.size[-1] < SIZE[-1]):
+        # TODO exception
+        raise Exception('size')
+    else:
+        os.remove(path=f'images/{name}')
+        # TODO rename
+        img.save(f'images/{name}', format=format, quality=50)
 
 
 class Category(models.Model):
@@ -34,7 +53,15 @@ class Post(models.Model):
     description: str = models.TextField(verbose_name='description')
     image: tp.IO = models.ImageField(verbose_name='image', upload_to='', null=True)
     published_date: datetime = models.DateTimeField(auto_now_add=True, null=True)
+
     # TODO change upload path
+    # image: quality: 50; size: (800, 768);
+
+    def save(self, *args, **kwargs) -> tp.Any:
+        super().save(*args, **kwargs)
+        if self.image is not None:
+            save_picture(image=self.image)
+
 
     def __str__(self) -> str:
         return f'pk: {self.pk}, category: {self.category.name}, title: {self.title}'
