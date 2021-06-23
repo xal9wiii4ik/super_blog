4c2eaa900a696c2f8e55be65588c9ee602259ed0
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-06-23 23:15:21 +0300

    add send email for activate user account, some fix in other files

--- blog/apps/user_profile/views.py
+++ blog/apps/user_profile/views.py
@@ -1,8 +1,11 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.parsers import MultiPartParser
 
 from apps.user_profile.models import Account
 from apps.user_profile.serializers import AccountModelSerializer
 from apps.user_profile.permmissions import IsAuthenticatedOrOwner
+from apps.user_profile.services_views import send_activation_email
 
 
 class UserProfileModelViewSet(ModelViewSet):
@@ -13,3 +16,11 @@ class UserProfileModelViewSet(ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountModelSerializer
     permission_classes = (IsAuthenticatedOrOwner,)
+    parser_classes = (MultiPartParser,)
+
+    def create(self, request, *args, **kwargs) -> Response:
+        request.data['is_active'] = False
+        print(request.data)
+        response = super(UserProfileModelViewSet, self).create(request, *args, **kwargs)
+        send_activation_email(request=request, data=response.data)
+        return response
