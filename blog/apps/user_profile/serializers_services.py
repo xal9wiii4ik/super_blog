f0d6b7c5af3fa0a5d42862d0e677db37b0b42849
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-06-30 12:23:21 +0300

    add reset_password

--- blog/apps/user_profile/serializers_services.py
+++ blog/apps/user_profile/serializers_services.py
@@ -3,15 +3,26 @@ from rest_framework.exceptions import ValidationError
 from apps.user_profile.models import Account
 
 
-def validate_email(email: str) -> None:
+def validate_email(email: str, action: str) -> int or None:
     """
-    Check email in exists users
-    :param email:
-    :return:
+    Checking email in exists users
     """
 
     try:
-        Account.objects.get(email=email)
-        raise ValidationError('email exist')
+        account = Account.objects.get(email=email)
+        # TODO check this with tests
+        if action == 'create_user':
+            raise ValidationError('email exist')
+        return account.id
     except Account.DoesNotExist as e:
-        pass
+        if action == 'reset_password':
+            raise ValidationError('user with this email not found')
+
+
+def validate_passwords(password: str, repeat_password: str) -> None:
+    """
+    Checking if password is similar repeat password
+    """
+
+    if password != repeat_password:
+        raise ValidationError('Password is not similar repeat password')
--- blog/apps/user_profile/views.py
+++ blog/apps/user_profile/views.py
@@ -5,11 +5,12 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.decorators import action
 
 from apps.user_profile.models import Account
-from apps.user_profile.serializers import AccountModelSerializer
+from apps.user_profile.serializers import AccountModelSerializer, ResetPasswordSerializer
 from apps.user_profile.permmissions import IsAuthenticatedOrOwner
-from apps.user_profile.services_views import send_activation_email, updating_account, send_updating_email
-    # update_email, activation_account
+from apps.user_profile.services_views import updating_account, send_updating_email
 
+
+# TODO override token auth(username and email auth)
 
 class UserProfileModelViewSet(ModelViewSet):
     """
@@ -20,6 +21,7 @@ class UserProfileModelViewSet(ModelViewSet):
     serializer_class = AccountModelSerializer
     permission_classes = (IsAuthenticatedOrOwner,)
     parser_classes = (MultiPartParser,)
+
     # TODO update tests
 
     def partial_update(self, request, *args, **kwargs) -> Response:
@@ -27,14 +29,17 @@ class UserProfileModelViewSet(ModelViewSet):
         if request.data.get('email') is not None:
             email = request.data.pop('email')
             response = super(UserProfileModelViewSet, self).partial_update(request, *args, **kwargs)
-            send_updating_email(request=request, data=response.data, action='update_account', email=email[0])
+            send_updating_email(request=request,
+                                data=response.data,
+                                action='update_account',
+                                email=email[0],
+                                updated_data=email[0])
         response = super(UserProfileModelViewSet, self).partial_update(request, *args, **kwargs)
         return Response(data=response.data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs) -> Response:
         request.data['is_active'] = False
         response = super(UserProfileModelViewSet, self).create(request, *args, **kwargs)
-        # send_activation_email(request=request, data=response.data)
         send_updating_email(request=request, data=response.data, action='activate_account')
         return response
 
@@ -47,7 +52,7 @@ class UserProfileModelViewSet(ModelViewSet):
         Url for activate account
         """
 
-        updating_account(uid=str(kwargs['uid']), user_id=int(kwargs['user_id']))
+        updating_account(uid=str(kwargs['uid']), user_id=int(kwargs['user_id']), action='activate_account')
         return Response(data={'ok': 'email has been updated successfully'}, status=status.HTTP_200_OK)
 
     @action(detail=False,
@@ -59,5 +64,33 @@ class UserProfileModelViewSet(ModelViewSet):
         Url for update fields in account(email)
         """
 
-        updating_account(uid=str(kwargs['uid']), user_id=int(kwargs['user_id']))
-        return Response(data={'ok': 'user has been activate successfully'}, status=status.HTTP_200_OK)
+        updating_account(uid=str(kwargs['uid']), user_id=int(kwargs['user_id']), action='update_account')
+        return Response(data={'ok': 'user has been updated successfully'}, status=status.HTTP_200_OK)
+
+    @action(detail=False,
+            methods=['POST'],
+            permission_classes=[permissions.AllowAny],
+            url_path=r'reset_password')
+    def reset_password(self, request, *args, **kwargs) -> Response:
+        """
+        Reset password if user forgot this
+        """
+        serializer = ResetPasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.data
+            send_updating_email(request=request,
+                                data=data,
+                                action='reset_password_confirm',
+                                updated_data=data['repeat_password'])
+            return Response(data={'ok': 'Check your email'}, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False,
+            methods=['GET'],
+            permission_classes=[permissions.AllowAny],
+            url_path=r'reset_password_confirm/(?P<uid>\w+)/(?P<user_id>\d+)')
+    def reset_password_confirm(self, request, *args, **kwargs) -> Response:
+        updating_account(uid=str(kwargs['uid']), user_id=int(kwargs['user_id']), action='reset_password')
+        return Response(data={'ok': 'Password has been updated successfully'}, status=status.HTTP_200_OK)
+
